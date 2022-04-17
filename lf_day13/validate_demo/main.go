58f7d1af2f3bc204443b45912d6604bd6907b63b
Jones0521 <2053921234@qq.com>
2022-04-17 09:41:10 +0800

    validate 数据校验

---
+++ lf_day13/validate_demo/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type Param struct {
+	Name    string `json:"name"`
+	Age     int    `json:"age"`
+	Married bool   `json:"married"`
+}
+
+func main() {
+	r := gin.Default()
+	r.POST("/test", testHandler)
+	r.Run()
+}
+func testHandler(c *gin.Context) {
+	var p Param
+	if err := c.ShouldBindUri(&p); err != nil {
+		fmt.Println(err)
+
+		c.JSON(http.StatusOK, "参数错误:"+err.Error())
+		return
+	}
+	fmt.Printf("p: %#v/n", p)
+	c.JSON(http.StatusOK, p)
+
+}
